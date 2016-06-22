3525eb26ac15903a5992da8c5b2bed6c5b273b3d
Raphael N <raphaelndwiga@musoni.co.ke>
2016-06-22 10:02:35 +0300

    Introduced new function _create_storage() for creating folder based on the day the script is run. Also modified the function reader_write() to save the generated .xls to the created folder and save the file path to the db.

--- application/controllers/Cash_flow.php
+++ application/controllers/Cash_flow.php
@@ -2,10 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cash_flow extends CI_Controller {
+	
+	public function __construct()
+        {
+                // Call the CI_Controller constructor
+                parent::__construct();
+				$this->load->model('Cash_flow_model', 'CashFlow');
+						$this->load->helper('url');
+						        //load the excel library
+				$this->load->library('excel');
+        }
+		
 public function index()
 	{
-	    //$this->reader_write(); //call the function that writes and saves the file. subtitute this with the one that receives data from webhook
-			$this->retrieve_file_info();
+	    $this->reader_write(); //call the function that writes and saves the file. subtitute this with the one that receives data from webhook
+		//	$this->_create_storage();
+		//$methods = get_class_methods('CI_Controller');
+		//print_r ($methods);
 	}
 public function receive_data()
 {
@@ -15,25 +28,17 @@ public function receive_data()
 	*/
 	$jsonArray = json_decode(file_get_contents('php://input'),true);
 }
-public function retrieve_file_info()
-	{
-			$this->load->helper('file');
-		$content = get_filenames(APPPATH.'controllers/', true);
-			print_r($content);
-	}
+
 public function reader_write()
 	{
 		/*
 			Introduce the PHPExcel Writer
 		*/
-		    date_default_timezone_set('Europe/London');
-			$today = date('Y-m-d'); //set the date to be used in saving the file. This has not been implemented yet
+		   // date_default_timezone_set('Europe/London');
+		   ini_set('date.timezone', 'UTC'); //setting the default timezone
 			$time = date('H:i:s');  //set the time
 		// $inputFileType = 'Excel5';
         $inputFile = './docs/flow-demo.xlsx';
-        //load the excel library
-        $this->load->library('excel');
-
         /**  Identify the type of $inputFileName  **/
         $inputFileType = PHPExcel_IOFactory::identify($inputFile);
         /**  Create a new Reader of the type defined in $inputFileType  **/
@@ -54,7 +59,8 @@ public function reader_write()
 						//while ($fileContent = json_decode($str_data, true)) { //rewrite this loop
 						//Setting Variables for the file
                            $baseRow = 6;
-						   $fileName= 'Flow-demo-Output';//$resfor=$dataname['name'];
+						    // Including the timestamp during the 
+						   $fileName= 'Flow-demo-Output - ' . date('m-d-Y_his') ;//$resfor=$dataname['name'];
 						//foreach loop to write data to file
 						   foreach($fileContent as $r => $dataRow) {
 									if (!is_array($dataRow)) {
@@ -80,7 +86,19 @@ public function reader_write()
 					   //echo 'I got here';
 					   //$objWriter->save('./docs/fin.xls');
 					   //exit;
-                       $objWriter->save(str_replace(__FILE__,'./docs/'.$fileName . '.xlsx',__FILE__));
+					   /*
+						Calling the function to create folder based on date. if it does not run successfully, the script terminates.
+					   */
+					  $createdFolder = $this->_create_storage() . '/';  //adding the slash to point to inside the dir
+					  $savedPath = $createdFolder . $fileName; //joining the created folder and the file name for the path
+					   
+                  //  $objWriter->save(str_replace(__FILE__,'./docs/'. $createdFolder . $fileName . '.xlsx',__FILE__));
+				  $objWriter->save(str_replace(__FILE__,'./docs/'. $savedPath . '.xlsx',__FILE__));
+						//Getting the file name to be saved in database
+					$savedFilePath = base_url() . 'docs/'.$savedPath. '.xlsx';
+								$this->CashFlow->save_file($savedFilePath);			
+				   echo $savedFilePath; //remove this line later on
+				   echo '<br>';
                        //$objWriter->save(str_replace('.php', '.xls', __FILE__));
                        //Set message to be written on the browser to confirm that the file has been written
                        echo date('H:i:s') , " File written to " , str_replace('.php', '.xlsx', pathinfo(__FILE__, PATHINFO_BASENAME));
@@ -89,5 +107,30 @@ public function reader_write()
                 //}
 
 	}
-
+	private function _create_storage()
+	{
+		/*
+		* This function is for creating folders organized by date for the storage of files
+		call this function before any file created to set the dependencies 
+		*/ 
+		$today = date('Y-m-d'); //setting the date	
+		if (!is_dir('docs'))
+			{
+				mkdir('./docs', 0777, true); //creating the folder docs if it does not already exist
+			}
+		if (!is_dir('docs/' . $today))
+			{
+				//creating folder based on day if it does not exist. If it does, it is not created
+				// mkdir('./docs/' . $today, 0777, true); 
+				if (!mkdir('./docs/' . $today, 0777, true)) {
+							die('Failed to create folders...'); // Die if the function mkdir cannot run
+					}
+				return $today;
+			} elseif (is_dir('docs/' . $today)){ //check if the folder is created and return it
+				return $today;
+			} else {
+				return $today; 				// Return the folder if its already created in the file system
+				//echo 'folder already exists';
+			}
+	}
 }
